9c2f37b83be76020666f0a6e1606766ef5ef3d09
tiszaszaki <jobljr9717@gmail.com>
2019-06-19 13:08:33 +0200

    update package.sh

--- package.sh
+++ package.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+SOURCE="flats/ graphics/ maps/ sprites/ textures/ DECORATE DEHACKED DOOMDEFS MAPINFO"
 rm -f ssd-remaster.pk3
-zip -r ssd-remaster.pk3 * -x README.md
+zip -r ssd-remaster.pk3 $SOURCE
