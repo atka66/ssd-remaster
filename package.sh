d68e4383b762cd77f65cc8179b921d478e2d15c7
tiszaszaki <jobljr9717@gmail.com>
2019-06-19 18:50:00 +0200

    update package.sh

--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SOURCE="flats/ graphics/ maps/ music/ sounds/ sprites/ textures/ ANIMDEFS DECALDEF DECORATE DEHACKED DOOMDEFS KEYCONF LOCKDEFS MAPINFO SNDINFO SNDSEQ"
-rm -f ssd-remaster.pk3
-zip -r ssd-remaster.pk3 $SOURCE
+ARCHIVE_PK3="ssd-remaster.pk3"
+if [ -f $ARCHIVE_PK3 ]; then rm $ARCHIVE_PK3; fi
+zip -r $ARCHIVE_PK3 $SOURCE
